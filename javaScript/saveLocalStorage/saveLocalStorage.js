744e8cc00c45c1a9866c8f3cbdef6ac7b4be24bf
smarchena <santy832@gmail.com>
2025-08-27 23:11:55 -0500

    bill changes

--- javaScript/saveLocalStorage/saveLocalStorage.js
+++ javaScript/saveLocalStorage/saveLocalStorage.js
@@ -4,15 +4,19 @@ var recuperar = document.getElementById("obtener");
 var h3 = document.getElementById("h3");
 
 guardar.addEventListener("click", function(){
-
-    localStorage.setItem("dato", nom.value);
-    
+    localStorage.setItem("dato", nom.value);    
 });
 
 recuperar.addEventListener("click", function(){
     h3.innerHTML = localStorage.getItem("dato");
 });
 
+localStorage.removeItem("dato")
+
+/* localStorage sirve para almacenar y recuperar datos que guardamos en nuestra computadora
+   localStorage.setItem("nombre_del_dato", "valor_del_dato") <-- para guardar
+   localStorage.getItem("nombre_del_dato") <-- para recuperar el dato que quiero
+*/
 
 /* Ejercicio:
 Guardar todos los datos que el usuario va ingresando, y cuando el quiera,
@@ -21,18 +25,17 @@ mostrar dichos datos. */
 var name1 = document.getElementById("name1");
 var save = document.getElementById("save");
 var restore = document.getElementById("restore");
-var h3 = document.getElementById("h3_2");
+var h3_2 = document.getElementById("h3_2");
 var datos = "";
 
 save.addEventListener("click", function(){
-    datos = datos + "<br>" + name1.value;
+    datos = datos + "<br>" + name1.value;    
     localStorage.setItem("saving", datos);
     name1.value = "";   
 });
 
 restore.addEventListener("click", function(){
-    h3.innerHTML = localStorage.getItem("saving");
-    
+    h3_2.innerHTML = localStorage.getItem("saving");    
 });
 
 localStorage.removeItem("saving");
--- tareas/factura/scriptBill.js
+++ tareas/factura/scriptBill.js
@@ -8,11 +8,17 @@ let descrip = document.getElementById("input_description");
 let price = document.getElementById("input_precio");
 let h3 = document.getElementById("h3");
 let h3_2 = document.getElementById("h3_2");
+let h3_cant = document.getElementById("h3_cant");
+let h3_cant_name = document.getElementById("h3_cant_name");
+let h3_descrip_name = document.getElementById("h3_descrip_name");
+let h3_precio_name = document.getElementById("h3_precio_name");
 let guardar = document.getElementById("save");
 let recuperar = document.getElementById("restore");
-let image = document.getElementById("img");
 let datos = "";
 let items = "";
+let cantidad = "";
+let descripcion = "";
+let precio = "";
 
 guardar.addEventListener("click", function(){
 
@@ -20,13 +26,27 @@ guardar.addEventListener("click", function(){
     <li><strong>Fecha: </strong>${dateInfo.value}</li>
     <li><strong>Nombre: </strong>${name1.value} ${lastname.value}</li>
     <li><strong>Teléfono: </strong>${number.value}</li>
-    <li><strong>Email: </strong>${email.value}</li><br>
-    </ul>`
-    
-    items = `<strong>Cantidad: </strong>${input_cant.value} <strong>Descripción: </strong>${input_description.value} <strong>Precio: </strong> ${input_precio.value}`
+    <li><strong>Email: </strong>${email.value}</li>
+    </ul>`;
+    nombreCantidad = `<strong>CANTIDAD</strong>`;
+    nombreDescripcion = `<strong>DESCRIPCIÓN</strong>`;
+    nombrePrecio = `<strong>PRECIO</strong>`;
+    cantidad = `${cantidad} <br> ${input_cant.value}`;
+    descripcion = `${descripcion} <br> -${input_description.value}`;
+    precio = `${precio} <br> $${input_precio.value}`;
+    let sumaCant = parseInt(cantidad) + parseInt(input_cant.value);
+    let sumaPrice = parseInt(precio) + parseInt(input_precio.value);
+    let totalPrice = sumaCant * sumaPrice;
+    total = `Total a pagar: ${totalPrice}`
 
-    localStorage.setItem("nombres", datos);
-    localStorage.setItem("items", items);
+    localStorage.setItem("nombres", datos);    
+    localStorage.setItem("nombreCantidad",nombreCantidad);
+    localStorage.setItem("nombreDescripcion",nombreDescripcion);
+    localStorage.setItem("nombrePrecio",nombrePrecio);
+    localStorage.setItem("cantidad", cantidad);
+    localStorage.setItem("descripcion", descripcion);
+    localStorage.setItem("precio", precio);
+    localStorage.setItem("precioTotal", total)
 
     cant.value = ''
     descrip.value = ''
@@ -34,8 +54,14 @@ guardar.addEventListener("click", function(){
 });
 
 recuperar.addEventListener("click", function(){
-    h3.innerHTML = localStorage.getItem("nombres");
-    h3_2.innerHTML = localStorage.getItem("items");
+    h3.innerHTML = localStorage.getItem("nombres");   
+    h3_cant.innerHTML = localStorage.getItem("cantidad");
+    h3_descrip.innerHTML = localStorage.getItem("descripcion");
+    h3_precio.innerHTML = localStorage.getItem("precio");
+    h3_cant_name.innerHTML = localStorage.getItem("nombreCantidad");
+    h3_descrip_name.innerHTML = localStorage.getItem("nombreDescripcion");
+    h3_precio_name.innerHTML = localStorage.getItem("nombrePrecio"); 
+    totalPrecio.innerHTML = localStorage.getItem("precioTotal");  
 
     name1.value = ''
     lastname.value = ''
@@ -44,5 +70,7 @@ recuperar.addEventListener("click", function(){
     date.value = ''
 });
 
-
+localStorage.removeItem("cantidad")
+localStorage.removeItem("descripcion")
+localStorage.removeItem("precio")
 
