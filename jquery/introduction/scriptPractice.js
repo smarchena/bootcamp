4d866a99983b77b074af2d35cad2346a4aef4546
smarchena <santy832@gmail.com>
2025-09-06 16:36:04 -0500

    practice jquery

---
+++ jquery/introduction/scriptPractice.js
@@ -0,0 +1,18 @@
+// javascript
+/*
+let dato_input = document.getElementById('dato');
+let boton = document.getElementById('boton');
+let res = document.getElementsByClassName('resultado');
+
+console.log(res[0].innerHTML)
+
+boton.addEventListener('click', function () {
+    res[1].innerHTML = dato_input.value;
+});
+*/
+
+// jquery
+
+$('#boton').click( function () {
+    alert('Hola');
+});
