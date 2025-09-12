699243e39b58aaaa2fb960b2160430516fc1b582
smarchena <santy832@gmail.com>
2025-09-11 22:15:38 -0500

    complete data blockbuster

--- jquery/task/data/script.js
+++ jquery/task/data/script.js
@@ -1,19 +1,29 @@
-$(document).ready( function () { 
+$(document).ready(function () {
+    $('#table').hide()
 
-    $('#button-addon2').click(function () {
-        let  val = $('#input1').val()
-        $('#table').append(`          
-                <tr>
-                    <ul id="ul">
-                        ${val}
-                    </ul>
-                    <th scope="row"></th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-               
-            `)
+    $('#button-add').click(function () {
+        let inputName = $('#nombre').val()
+        let gender = $('#select').val()
+        let inputYear = $('#año').val()
+        let inputMoney = $('#valor').val()
+
+        $('#table').show().append(`            
+            <tr class='child'>
+                <th>${inputName}</th>
+                <th>${gender}</th>
+                <th>${inputYear}</th>
+                <th>$${inputMoney}</th>                
+            </tr>`
+        )
+
+        $('#nombre').val('')
+        $('#select').prop('selectedIndex', 0)
+        $('#año').val('')
+        $('#valor').val('')
+    })
+
+    $('#button-clean').click(function () {
+        $('.child').remove()
+        $('#table').hide()
     })
 })
