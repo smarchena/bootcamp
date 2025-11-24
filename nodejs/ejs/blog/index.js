ebb2129d91e3655723d48f636e7179db23f489b3
smarchena <santy832@gmail.com>
2025-11-23 22:07:38 -0500

    blog cards advances

--- nodejs/ejs/blog/index.js
+++ nodejs/ejs/blog/index.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const Blog = require('./src/models/Blog')
 
+
 //configuraciones
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.set('views', __dirname + '/src/views')
-app.set('views engine', 'ejs')
+let path = __dirname + "/src/views"
+app.set("views", path)
+app.set("view engine", "ejs")
 
 mongoose.connect('mongodb+srv://user:<password>@cluster0.pvycuuk.mongodb.net/blog')
     .then(function (db) {
@@ -20,7 +22,7 @@ mongoose.connect('mongodb+srv://user:<password>@cluster0.pvycuuk.mongodb.net/blo
 
 app.get('/inicio', async function (req, res){
     var docs = await Blog.find();
-    res.sendFile(__dirname + './src/views/index')
+    res.render('index', {titulo: 'este es el título'})
 })
 
 
--- nodejs/ejs/blog/src/models/Blog.js
+++ nodejs/ejs/blog/src/models/Blog.js
@@ -2,6 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Blog = new Schema({
+     nombre: String,
+  descripcion: {
+    type: String,
+    default: "...",
+  },
+  prioridad: String,
+  estado: {
+    type: Boolean,
+    default: false,
+  },
     
 })
 
