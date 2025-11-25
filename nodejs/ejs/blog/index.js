62dfa707beee0bd24158635dba823f83d6d78175
smarchena <santy832@gmail.com>
2025-11-25 00:11:06 -0500

    blog edit and create buttoms functioning correctly and connected with database mongodb correctly

--- nodejs/ejs/blog/index.js
+++ nodejs/ejs/blog/index.js
@@ -4,8 +4,6 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const Blog = require('./src/models/Blog')
 
-
-//configuraciones
 app.use(bodyParser.urlencoded({ extended: true }))
 
 let path = __dirname + "/src/views"
@@ -20,12 +18,42 @@ mongoose.connect('mongodb+srv://user:<password>@cluster0.pvycuuk.mongodb.net/blo
         console.log(`Ups! Ocurrió un problema ${err}`)
     })
 
-app.get('/inicio', async function (req, res){
-    /* var docs = await Blog.find(); */
-    res.render('index', {titulo: 'este es el título'})
+app.get('/inicio', async function (req, res){   
+    let blogs = await Blog.find() 
+    res.render('index', {titulo: 'este es el título', blog: blogs, url: Blog.findById(req.params.id)})
 })
 
+app.get('/create', async function (req, res) {     
+    res.render('_create')   
+})
 
+app.post('/create', async function(req,res){        
+    let datos = req.body
+    let new_blog = new Blog(datos)
+    new_blog.save()
+    console.log('Guardado en la base de datos: ok!')
+    res.redirect('/inicio')
+})
+
+app.get('/edit/:id_card', async function(req,res){    
+    let id = req.params.id_card    
+    let card = await Blog.findById(id)
+    let blogs = await Blog.find()
+
+    res.render('_edit', {
+        titulo: `Edit card named "${card.title}"`,
+        blog: blogs,
+        card: card,
+        url: card.url
+    })
+})
+
+app.post('/edit/:id_card', async function(req, res){
+    let id = req.params.id_card
+    let card = await Blog.updateOne({ _id: id }, req.body);
+    console.log('Actualizado con éxito.')
+    res.redirect('/inicio')
+})
 
 app.listen(3000)
 
--- nodejs/ejs/blog/src/models/Blog.js
+++ nodejs/ejs/blog/src/models/Blog.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Blog = new Schema({
- 
+    title : String,
+    url : String,
+    description : String,
 })
 
 module.exports = mongoose.model('blog', Blog)
